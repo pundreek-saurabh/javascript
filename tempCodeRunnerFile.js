fc6988dbd3bf280f16718cc08b10046fd7799e95
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-07-13 05:57:36 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let fullNameMaker= function(fname,lname)
-// {
-//     console.log('welcome to lco');
-//     console.log(`greetings mr. ${fname} ${lname}`);
-// }
-
-// fullNameMaker('jane', 'doe')
