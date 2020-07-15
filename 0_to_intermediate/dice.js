e82decc0ebab14f2a7f4cc88b59aefb4255cc122
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-07-15 11:13:31 +0530

    Add files via upload

---
+++ 0_to_intermediate/dice.js
@@ -0,0 +1,16 @@
+// let numOne = 22
+// let numTwo = 7
+
+// result =22/7
+// console.log(result.toFixed(2)); // ".toFixed" is a method used to get the result upto given place of decimal
+// let floatRes = result.toFixed(2)
+// console.log(Math.floor(floatRes));
+// console.log(Math.ceil(floatRes));
+
+//MATH IS A INBUILT OBJECT IN JAVASCRIPT.
+
+let result =Math.random()*6+1; //"random" method in "Math" object  gives the random number between 0 to 1.
+console.log(Math.floor(result));
+
+let resultTwo = Math.random()*6;
+console.log(Math.ceil(resultTwo));
