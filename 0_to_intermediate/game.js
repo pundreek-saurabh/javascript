295cdbcd49d6cfb5d457996d23503c5a1d388d61
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-07-15 11:33:18 +0530

    Add files via upload

---
+++ 0_to_intermediate/game.js
@@ -0,0 +1,17 @@
+//SPIN AND WIN FRONTEND PARt
+<html>
+    <head>
+        <title>game</title>
+    </head>
+
+    <body>
+        <script>
+
+        </script>
+    </body>
+</html>
+
+// MAJOR STEPS:-
+ // 1. load assets (images , sounds fonts etc.)
+ // 2. create updates 
+ // 3. update till the game is not over .
